219f86458ba63599004fe0fad4f7376db6b3c249
DixinFan <dixin.fan@outlook.com>
2020-02-08 17:20:57 +0800

    Finishing uploading video.

---
+++ src/view/upload-video-page.vue
@@ -0,0 +1,17 @@
+<template>
+    <Upload
+        multiple
+        type="drag"
+        name="video"
+        action="//localhost:8081/index.php/index/index/upload/">
+        <div style="padding: 20px 0">
+            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+            <p>Click or drag files here to upload</p>
+        </div>
+    </Upload>
+</template>
+<script>
+export default {
+
+}
+</script>
